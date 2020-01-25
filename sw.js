ad430ad503d6c6973d45436a1a97947c3a4c403e
choijooyoung <4906cjy@naver.com>
2020-01-25 18:43:16 +0900

    D

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('v1').then(function (cache) {
-=======
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
->>>>>>> a5ecdf37fc1e62dc1144374231f49c358d4e1596
       return cache.addAll([
         '/costcocoupon/',
         '/costcocoupon/index.html',
@@ -15,27 +9,19 @@ self.addEventListener('install', function(event) {
         '/costcocoupon/coupon.js',
         '/costcocoupon/coupon.html',
         '/costcocoupon/manifest.json',
-<<<<<<< HEAD
         '/costcocoupon/logo/logo1.png',
         '/costcocoupon/logo/logo2.png',
         '/costcocoupon/logo/logo3.png',
         '/costcocoupon/logo/logo4.png',
         '/costcocoupon/logo/logo5.png',
-=======
->>>>>>> a5ecdf37fc1e62dc1144374231f49c358d4e1596
         '/costcocoupon/lunar.js'
       ]);
     })
   );
 });
 
-<<<<<<< HEAD
 self.addEventListener('fetch', function (event) {
   event.respondWith(caches.match(event.request).then(function (response) {
-=======
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
->>>>>>> a5ecdf37fc1e62dc1144374231f49c358d4e1596
     // caches.match() always resolves
     // but in case of success response will have value
     if (response !== undefined) {
@@ -46,11 +32,6 @@ self.addEventListener('fetch', function(event) {
         // we need to save clone to put one copy in cache
         // and serve second one
         let responseClone = response.clone();
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> a5ecdf37fc1e62dc1144374231f49c358d4e1596
         caches.open('v1').then(function (cache) {
           cache.put(event.request, responseClone);
         });
@@ -60,8 +41,4 @@ self.addEventListener('fetch', function(event) {
       });
     }
   }));
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> a5ecdf37fc1e62dc1144374231f49c358d4e1596
